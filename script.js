83ec2d42b9fafbd886f8ea9917471997b0f56d65
Sushanth <117469085+Sushanth-Hebri@users.noreply.github.com>
2023-10-28 21:51:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //TMDB API
-const API_KEY = 'put your access key here:) - sushanth-hebri';
+const API_KEY = 'api_key=b36a1de48e5008bd653343edc34d45e5';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by_popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
